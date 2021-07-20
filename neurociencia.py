9d35c15ad9c7db15608a8dbdc5c370e4635213fe
LuisAtzin <luisfrancoreyna@outlook.com>
2021-07-20 00:05:27 -0500

    Agregue la función calcular_td para calcular las tasas de disparo

--- neurociencia.py
+++ neurociencia.py
@@ -4,6 +4,9 @@ import numpy as np
 class neurociencia:
     
     def __init__(self):
+        
+    #Calcular la tasa de disparo de un archivo dado
+    def calcular_td(archivo, longitud, paso):
             
             
 
