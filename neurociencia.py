3d06d93e63d7337b82844df7ac3d5e08cf9bebd8
luisatzin <luisfrancoreyna@ciencias.unam.mx>
2021-11-04 21:21:08 -0600

    Function name refactor "homogeneizar".

--- neurociencia.py
+++ neurociencia.py
@@ -2,60 +2,59 @@
 import numpy as np
 import pandas as pd
 
-def homogenizar(archivo):
+
+def homogeneizar(archivo):
     numero_columnas = 0
-    
-    with open(archivo, 'r') as texto:
+    with open(archivo, "r") as texto:
         # Leer las líneas
         lineas = texto.readlines()
-    
+
         for linea in lineas:
-            #Contar número de columnas en linea
+            # Contar número de columnas en linea
             contador = len(linea.split(',')) + 1
-    
+
             if numero_columnas < contador:
                 numero_columnas = contador
-    
+
     # Cerrar el archivo
-    texto.close()   
-    
+    texto.close()
+
     # Generar el índice de columnas
     nombre_columnas = [i for i in range(0, numero_columnas)]
-    
+
     datos_neurona = pd.read_csv(archivo, header=None, delimiter=',',
-                                skip_blank_lines=False, names=nombre_columnas)    
+                                skip_blank_lines=False, names=nombre_columnas)
 
     data = datos_neurona.to_numpy()
     return data
-    
-#Calcular la tasa de disparo de un archivo dado
+
+
+# Calcular la tasa de disparo de un archivo dado
 def calcular_td(data, longitud, paso, tiempo_minimo, tiempo_maximo,
                 numero_clases):
-    
-    
     indices = [0 for x in range(numero_clases + 1)]
-    
+
     numero_ensayos, numero_columnas = data.shape
-    
-    #Obtener los indices para los cambios de clase
+
+    # Obtener los indices para los cambios de clase
     indices[0] = -1
     indices[numero_clases] = numero_ensayos
     j = 1
     for i in range(numero_ensayos):
-        if  np.isnan(data[i,1]):
+        if np.isnan(data[i, 1]):
             indices[j] = i
-            j += 1 
-    
-    #Dividir entre el paso para obtener última ventana
+            j += 1
+
+            # Dividir entre el paso para obtener última ventana
     data = np.floor_divide(data, paso)
     ultima = np.nanmin(data)
-    
-    ventanas = int(longitud//paso)
-    
-    #Calcular tiempos
+
+    ventanas = int(longitud // paso)
+
+    # Calcular tiempos
     tiempos_paso = []
     tiempos_grafica = []
-        
+
     tiempo = tiempo_minimo + (longitud / 2)
     while tiempo <= tiempo_maximo - (longitud / 2):
         tiempos_paso.append(tiempo)
@@ -63,17 +62,17 @@ def calcular_td(data, longitud, paso, tiempo_minimo, tiempo_maximo,
         tiempo += paso
 
     tasas_disparo = np.zeros((numero_clases, len(tiempos_grafica)))
-    
+
     for i in range(numero_clases):
-        ensayos_clase = data[indices[i] + 1 : indices[i + 1], :]
+        ensayos_clase = data[indices[i] + 1: indices[i + 1], :]
         k = ultima
         for j in range(len(tiempos_paso)):
             suma = 0
             for m in range(ventanas + 1):
                 sumandos = np.where(ensayos_clase == (k - m), 1, 0)
                 suma += np.sum(sumandos)
-            promedio = (suma) / (len(ensayos_clase))
+            promedio = suma / (len(ensayos_clase))
             tasa_disparo = promedio / longitud
-            tasas_disparo[i,j] = tasa_disparo
-            k+=1
+            tasas_disparo[i, j] = tasa_disparo
+            k += 1
     return tasas_disparo, tiempos_grafica
